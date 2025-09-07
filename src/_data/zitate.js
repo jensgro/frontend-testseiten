0cae4d88766c0b3bc374c68f589250cc0d4b724d
Jens Grochtdreis <jens@grochtdreis.de>
2025-09-07 13:07:48 +0200

    Zitate zu den Datenfiles hinzugefügt

---
+++ src/_data/zitate.js
@@ -0,0 +1,42 @@
+module.exports = [
+    {
+        quote: "Diplomatie ist die Kunst, mit hundert Worten zu verschweigen, was man mit einem einzigen Wort sagen könnte.",
+        author: "Saint-John Perse"
+    },
+    {
+        quote: "Die Mutter der Dummheit ist ständig schwanger.",
+        author: "Pelzig"
+    },
+    {
+        quote: "Ich freue mich, wenn es regnet, denn wenn ich mich nicht freue, regnet es auch.",
+        author: "Karl Valentin"
+    },
+    {
+        quote: "Ein Laster braucht der Mensch. Wer mehrere hat, zählt als Logistik-Unternehmen.",
+        author: "Frederic Hemberger"
+    },
+    {
+        quote: "Hört auf mit den Rauchern zu streiten, schliesst Frieden mit den Todgeweihten.",
+        author: "Ingo Insterburg"
+    },
+    {
+        quote: "Erfahrung heißt gar nichts. Man kann seine Sache auch 35 Jahre schlecht machen.",
+        author: "Kurt Tucholsky"
+    },
+    {
+        quote: "Das Leben ist ein ständiges Geben und Nehmen. Wir nehmen nicht nur, sondern lassen uns auch geben.",
+        author: "Mombacher Fassenacht, 2017"
+    },
+    {
+        quote: "Bin ich vom Pech verfolgt oder laufen wir nur zufällig in die gleiche Richtung?",
+        author: "Bodo Bach"
+    },
+    {
+        quote: "Wenn wir wüssten, was wir tun, wäre es keine Forschung.",
+        author: "Albert Einstein"
+    },
+    {
+        quote: "Es sind die Begegnungen mit den Menschen, die das Leben so trübsinnig machen.",
+        author: "Jack London"
+    }
+];
