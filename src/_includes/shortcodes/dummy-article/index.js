d5e31c2ce967316a811e3468b52d83bcef9eb44d
Jens Grochtdreis <jens@grochtdreis.de>
2025-11-08 15:44:57 +0100

    dummyarticle kann jetzt auch eine Klasse entgegennehmen

--- src/_includes/shortcodes/dummy-article/index.js
+++ src/_includes/shortcodes/dummy-article/index.js
@@ -1,12 +1,13 @@
-const dummyArticle = (count, image = false) => {
-    // const n = Math.floor(Number(count));
+const dummyArticle = (count, image = false, myclass = '') => {
     const n = count;
     let hrs = '';
     for (let i = 0; i < n; i++) {
         hrs += (i === 0 && image) ? '<hr class="image">' : '<hr>';
     }
 
-    return `<div class="dummy-article">
+    let theclass = 'dummy-article' + (myclass ? ' ' + myclass : '');
+
+    return `<div class="${theclass}">
       ${hrs}
     </div>
     `;
